ba5891c05e7a864e71bd5f238f84ba2d9e85c40a
Aleksandr Khokhlov <hohlovap@gmail.com>
2025-07-05 07:37:16 +0300

    added cnnection to sqlite database

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import os
 from flask import Flask, jsonify
 from prometheus_flask_exporter import PrometheusMetrics
+from sqlalchemy import create_engine, text
+
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
+
+db_url = f'sqlite:///{os.path.dirname(__file__)}/updater.db'
+engine = create_engine(db_url, connect_args={'check_same_thread': False}, echo=True)
+
 
 
 @app.route('/')
@@ -17,5 +24,13 @@ def health():
     return resp
 
 
+def select_all(_engine, table: str):
+    with _engine.connect() as conn:
+        result = conn.execute(text(f"SELECT * FROM {table}"))
+        for row in result:
+            print(row)
+
+
 if __name__ == '__main__':
+    select_all(engine, 'issues')
     app.run(debug=True)
